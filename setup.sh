80c02f531683618e5d6f113e2922904e22741348
Josh Gitlin <jgitlin@pinnacle21.com>
2020-04-22 12:58:45 -0400

    Fix dependency list
    * Need shadow-utils for useradd
    * Need python3 for ATS Build

--- setup.sh
+++ setup.sh
@@ -7,13 +7,16 @@ set -x
 #yum -y groupinstall "Development Tools" && \
 
 # rpmbuild command recommends to use `builder:builder` as user:group.
+/bin/yum -y install shadow-utils
 /usr/sbin/useradd -u 1000 builder
 
 mkdir -p /home/builder/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 chown -R builder:builder /home/builder/rpmbuild
 
 # Install Apache Traffic Server dependencies
-/bin/yum install autoconf automake libtool pkgconfig perl-ExtUtils-MakeMaker gcc openssl-devel pcre-devel ncurses-devel libcurl-devel libcap-devel hwloc-devel flex git
+/bin/yum -y install autoconf automake libtool pkgconfig perl-ExtUtils-MakeMaker \
+    gcc openssl-devel pcre-devel ncurses-devel libcurl-devel libcap-devel \
+    hwloc-devel flex git python3
 
 # Clean up for smaller image size
 /bin/yum clean all && rm -rf /var/cache/yum
