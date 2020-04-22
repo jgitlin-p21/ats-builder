c3e29009503370b789d783927f93847375fa3775
Josh Gitlin <jgitlin@pinnacle21.com>
2020-04-21 22:48:24 -0400

    Fix paths

--- setup.sh
+++ setup.sh
@@ -7,14 +7,14 @@ set -x
 #yum -y groupinstall "Development Tools" && \
 
 # rpmbuild command recommends to use `builder:builder` as user:group.
-useradd -u 1000 builder
+/usr/sbin/useradd -u 1000 builder
 
 mkdir -p /home/builder/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 chown -R builder:builder /home/builder/rpmbuild
 
 # Install Apache Traffic Server dependencies
-yum install autoconf automake libtool pkgconfig perl-ExtUtils-MakeMaker gcc openssl-devel pcre-devel ncurses-devel libcurl-devel libcap-devel hwloc-devel flex git
+/bin/yum install autoconf automake libtool pkgconfig perl-ExtUtils-MakeMaker gcc openssl-devel pcre-devel ncurses-devel libcurl-devel libcap-devel hwloc-devel flex git
 
 # Clean up for smaller image size
-yum clean all && rm -rf /var/cache/yum
-rm /tmp/setup.sh
+/bin/yum clean all && rm -rf /var/cache/yum
+rm -f /tmp/setup.sh
