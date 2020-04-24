fb4bae3fb9ecc789fdcf9bc16e2ec10504b531a0
Josh Gitlin <jgitlin@pinnacle21.com>
2020-04-23 21:09:00 -0400

    Adding additional missing dependencies

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Install Development tools
-#yum -y groupinstall "Development Tools" && \
+yum -y groupinstall "Development Tools"
 
 # rpmbuild command recommends to use `builder:builder` as user:group.
 /bin/yum -y install shadow-utils
@@ -17,7 +17,7 @@ chown -R builder:builder /home/builder/rpmbuild
 /bin/yum -y install autoconf automake libtool pkgconfig perl-ExtUtils-MakeMaker \
     gcc openssl-devel pcre-devel ncurses-devel libcurl-devel libcap-devel \
     hwloc-devel flex git python3 rpmdevtools rpm-build rpmlint gnupg2 sudo \
-    boost-devel tcl-devel xz-devel yaml-cpp-devel
+    boost-devel tcl-devel xz-devel gcc-c++ yaml-cpp-devel
 
 # Clean up for smaller image size
 /bin/yum clean all && rm -rf /var/cache/yum
