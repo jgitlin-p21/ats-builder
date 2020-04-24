31dbc4b065390a36c925ac07549dbaa091abdfd4
Josh Gitlin <jgitlin@pinnacle21.com>
2020-04-23 21:48:17 -0400

    Back out yaml-cpp-devel work

--- setup.sh
+++ setup.sh
@@ -6,11 +6,6 @@ set -x
 # Install Development tools
 yum -y groupinstall "Development Tools"
 
-# Install EPEL, needed for yaml-cpp-devel
-yum -y install yum-utils
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum-config-manager --enable epel
-
 # rpmbuild command recommends to use `builder:builder` as user:group.
 /bin/yum -y install shadow-utils
 /usr/sbin/useradd -u 1000 builder
@@ -22,7 +17,7 @@ chown -R builder:builder /home/builder/rpmbuild
 /bin/yum -y install autoconf automake libtool pkgconfig perl-ExtUtils-MakeMaker \
     gcc openssl-devel pcre-devel ncurses-devel libcurl-devel libcap-devel \
     hwloc-devel flex git python3 rpmdevtools rpm-build rpmlint gnupg2 sudo \
-    boost-devel tcl-devel xz-devel gcc-c++ yaml-cpp-devel
+    boost-devel tcl-devel xz-devel gcc-c++
 
 # Clean up for smaller image size
 /bin/yum clean all && rm -rf /var/cache/yum
